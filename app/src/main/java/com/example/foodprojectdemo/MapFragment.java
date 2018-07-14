38f57c99678511a1428e12d83351b802812115d4
AUser000 <dhanuhmd80@gmail.com>
2018-07-14 23:09:16 +0530

    add map styles

--- app/src/main/java/com/example/foodprojectdemo/MapFragment.java
+++ app/src/main/java/com/example/foodprojectdemo/MapFragment.java
@@ -1,16 +1,19 @@
 package com.example.foodprojectdemo;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-;
+import com.google.android.gms.maps.model.MapStyleOptions;
+;import static android.content.ContentValues.TAG;
 
 /**
  * Created by Dhanushka Dharmasena on 13/07/2018.
@@ -37,6 +40,14 @@ public class MapFragment extends Fragment implements com.google.android.gms.maps
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        map = googleMap;
+        try {
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getActivity().getApplicationContext(), R.raw.style_json));
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
     }
 }
