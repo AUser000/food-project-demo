aeb206b5f11f4459a7ebedf468db6a2e73d281f3
AUser000 <dhanuhmd80@gmail.com>
2018-07-15 08:48:04 +0530

    add FA buttons

--- app/src/main/java/com/example/foodprojectdemo/MapFragment.java
+++ app/src/main/java/com/example/foodprojectdemo/MapFragment.java
@@ -4,11 +4,17 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -20,7 +26,9 @@ import com.google.android.gms.maps.model.MapStyleOptions;
  */
 public class MapFragment extends Fragment implements com.google.android.gms.maps.OnMapReadyCallback {
     GoogleMap map;
-
+    FloatingActionButton fabM1, fabM2, fabM3;
+    Animation fabOpen, fabClose, fabRotateForward, fabRotateBackWord;
+    boolean fabIsOpen = false;
     public MapFragment() {}
 
     @Override
@@ -28,6 +36,7 @@ public class MapFragment extends Fragment implements com.google.android.gms.maps
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.map_fragment, container, false);
+        //fabM1 = (FloatingActionButton)
         return v;
     }
 
@@ -36,6 +45,38 @@ public class MapFragment extends Fragment implements com.google.android.gms.maps
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map1);
         mapFragment.getMapAsync(this);
+
+        fabM1 = (FloatingActionButton) view.findViewById(R.id.fabM1);
+        fabM2 = (FloatingActionButton) view.findViewById(R.id.fabM2);
+        fabM3 = (FloatingActionButton) view.findViewById(R.id.fabM3);
+
+        fabOpen = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_close);
+        fabRotateForward = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rotate_forword);
+        fabRotateBackWord = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rotate_backword);
+
+        fabM1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFab();
+            }
+        });
+
+        fabM2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFab();
+                Toast.makeText(getActivity().getApplicationContext(), "location btn function", Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        fabM3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFab();
+                Toast.makeText(getActivity().getApplicationContext(), "filter btn function", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -48,6 +89,24 @@ public class MapFragment extends Fragment implements com.google.android.gms.maps
             }
         } catch (Resources.NotFoundException e) {
             Log.e(TAG, "Can't find style. Error: ", e);
+        }
+    }
+
+    private void animateFab() {
+        if(fabIsOpen) {
+            fabM1.startAnimation(fabRotateForward);
+            fabM2.startAnimation(fabClose);
+            fabM3.setAnimation(fabClose);
+            fabM2.setClickable(false);
+            fabM3.setClickable(false);
+            fabIsOpen = false;
+        } else {
+            fabM1.startAnimation(fabRotateBackWord);
+            fabM2.startAnimation(fabOpen);
+            fabM3.setAnimation(fabOpen);
+            fabM2.setClickable(true);
+            fabM3.setClickable(true) ;
+            fabIsOpen = true;
         }
     }
 }
